f8488e9907cb30dffc585bc301d547b4d8d0bde9
Pankajashree <pankajashree@gmail.com>
2017-04-12 19:04:04 +0530

    Add custom style

--- applications/starbase/src/main.js
+++ applications/starbase/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//create vue instance to render the template from App.vue
+
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    //render method renders the App.vue component on index.html
+    //it takes one parameter( h is general name) that takes care of creating the element actual rendering
+    render: h => h(App)
 })
