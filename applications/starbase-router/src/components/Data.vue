3cebedf3229be65407fa5de80700f84d44096371
Pankajashree <pankajashree@gmail.com>
2017-04-12 23:27:30 +0530

    add router

---
+++ applications/starbase-router/src/components/Data.vue
@@ -0,0 +1,16 @@
+<template>
+<div class = ''>
+Type: {{type}}
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            //$route is from vue-router
+            type: this.$route.params.type
+        }
+    }
+}
+</script>
