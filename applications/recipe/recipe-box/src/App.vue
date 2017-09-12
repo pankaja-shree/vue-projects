482f67ec4d55bc317c7d38169237101bf7f09fc3
pankaja-shree <pankajashree@gmail.com>
2017-09-12 16:20:11 +0530

    Add recipe box

---
+++ applications/recipe/recipe-box/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+  Recipe box
+  <form>
+  <input v-model="dish.name"  type = "text" placeholder = "Recipe Name" required>
+  <textarea  v-model="dish.ingredients" rows = "5" cols = "50" placeholder = "Enter ingredients separated by Commas"></textarea><br>
+  <input type = "button" @click = "addrecipe" value = "Add new recipe">
+  </form>
+    <Recipe :recipe-item = "recipes"></Recipe>
+  </div>
+</template>
+
+<script>
+import Recipe from './components/recipe'
+export default {
+  name: 'app',
+  components: {
+    Recipe
+  },
+  data () {
+    return {
+      dish: {
+      },
+      recipes: [
+        {
+          name: 'Pumpkin Pie',
+          ingredients: 'Pumpkin Puree,Sweetened Condensed Milk,Eggs,Pumpkin Pie Spice,Pie Crust'
+        },
+        {
+          name: 'Spaghetti',
+          ingredients: 'Noodles,Tomato Sauce,(Optional) Meatballs'
+        }
+      ]
+    }
+  },
+  methods: {
+    addrecipe () {
+      return this.recipes.push(this.dish)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+input{
+  margin: 1% auto;
+  padding: 1%;
+  display: block;
+}
+</style>
---
+++ applications/recipe/recipe-box/src/components/recipe.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="recipe">
+  <div v-for = "recipe in recipeItem">
+    <h2>{{recipe.name}}</h2>
+    <h3>{{recipe.ingredients}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'recipe',
+  props: ['recipeItem']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
