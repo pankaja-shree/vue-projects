fcd2524b01c37c83ed7bbf6ceb4b8d8a5d488875
pankaja-shree <pankajashree@gmail.com>
2017-09-15 16:54:59 +0530

    import normalize and vue-material css

--- applications/recipe/recipe-box/src/main.js
+++ applications/recipe/recipe-box/src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import 'normalize.css'
+import 'vue-material/dist/vue-material.css'
 var VueMaterial = require('vue-material')
 
 Vue.use(VueMaterial)
