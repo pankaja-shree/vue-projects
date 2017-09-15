614161758111e00d7a1c7ac5b99f5c28e4aab8db
pankaja-shree <pankajashree@gmail.com>
2017-09-15 16:54:39 +0530

    use modal component in app

--- applications/recipe/recipe-box/src/App.vue
+++ applications/recipe/recipe-box/src/App.vue
@@ -1,58 +1,18 @@
 <template>
-  <div id="app">
-  Recipe box
-  <form>
-  <input autofocus v-model="dish.name"  type = "text" placeholder = "Recipe Name" required>
-  <textarea required v-model="dish.ingredients" rows = "5" cols = "50" placeholder = "Enter ingredients separated by Commas"></textarea><br>
-  <input type = "button" @click = "addrecipe" @keyup.enter = "addrecipe" value = "Add new recipe">
-  </form>
-    <Recipe :recipe-item = "recipes"></Recipe>
-  </div>
+<div id = 'app'>
+<md-toolbar class="md-medium md-accent">
+  <h1 class="md-title" style="flex: 1"><b>FCC Data Visualization Project 3: Recipe box</b></h1>
+  <modal></modal>
+</md-toolbar>
+</div>
 </template>
 
 <script>
-import Recipe from './components/recipe'
+import modal from './components/modal.vue'
 export default {
   name: 'app',
   components: {
-    Recipe
-  },
-  data () {
-    return {
-      dish: {
-      },
-      recipes: [
-        {
-          name: 'Pumpkin Pie',
-          ingredients: 'Pumpkin Puree,Sweetened Condensed Milk,Eggs,Pumpkin Pie Spice,Pie Crust'
-        },
-        {
-          name: 'Spaghetti',
-          ingredients: 'Noodles,Tomato Sauce,(Optional) Meatballs'
-        }
-      ]
-    }
-  },
-  methods: {
-    addrecipe: function () {
-      return this.recipes.push(this.dish)
-    }
+    modal
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-input{
-  margin: 1% auto;
-  padding: 1%;
-  display: block;
-}
-</style>
