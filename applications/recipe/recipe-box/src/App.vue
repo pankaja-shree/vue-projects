6050475c540ae492fb9c8fa494a714433bfe6f7f
pankaja-shree <pankajashree@gmail.com>
2017-09-15 17:03:56 +0530

    use recipe component

--- applications/recipe/recipe-box/src/App.vue
+++ applications/recipe/recipe-box/src/App.vue
@@ -4,15 +4,19 @@
   <h1 class="md-title" style="flex: 1"><b>FCC Data Visualization Project 3: Recipe box</b></h1>
   <modal></modal>
 </md-toolbar>
+<recipes></recipes>
 </div>
 </template>
 
 <script>
 import modal from './components/modal.vue'
+import recipes from './components/recipe.vue'
+
 export default {
   name: 'app',
   components: {
-    modal
+    modal,
+    recipes
   }
 }
 </script>
