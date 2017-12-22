3494d38ba2bd7af98ea5ccf4ae798daa424c6df4
Pankajashree <pankajashree@gmail.com>
2017-12-22 14:05:34 +0530

    insert buttons and irebase code

--- applications/loginpage/src/components/Signup.vue
+++ applications/loginpage/src/components/Signup.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="signup">
-    <v-btn large left>Signup with Google</v-btn>
-    <v-btn large left>Signup with Github</v-btn>
-    <v-btn large left>Signup with Twitter</v-btn>
+    <button @click="signUp()">Signup with Google</button>
+    <button>Signup with Github</button>
+    <button>Signup with Twitter</button>
     <br>
         <p>{{error.message}}</p>
         <br>
@@ -11,8 +11,41 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+import {firebaseApp} from '../firebaseApp'
+
 export default {
-  name: 'signup'
+  data() {
+    return {
+      error: {
+          message: ''
+        }
+      }
+   },
+  methods: {
+    signUp() {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+      }
+   }
 }
 </script>
 
