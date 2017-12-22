fec78ea90dc9cfb2d32633233e8b94781ecccd1c
pankaja-shree <pankajashree@gmail.com>
2017-12-22 11:01:26 +0530

    import router and firebase

--- applications/loginpage/src/main.js
+++ applications/loginpage/src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import {firebaseApp} from './firebaseApp'
+
+//firbase listens if user has signed in or out on any path of the app - always before Vue instance
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+      store.dispatch('signIn',user) // call the action signIn with the user parameter
+      router.push('/dashboard') //routerpush method will navigate to this page and place this page on top (last of the stack)
+  }
+  else {
+      router.replace('/signin') //replace will place this page on the beginning of the stack
+  }
+})
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
