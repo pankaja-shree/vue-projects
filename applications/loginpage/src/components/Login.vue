63fa4577da9d9e252dc90e7100132a0c8c8c0850
pankaja-shree <pankajashree@gmail.com>
2017-12-22 12:28:23 +0530

    handle google auth

--- applications/loginpage/src/components/Login.vue
+++ applications/loginpage/src/components/Login.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="login">
-    <button>Login with Google</button>
-    <button large left>Login with Github</button>
-    <button large left>Login with Twitter</button>
+    <button @click="logIn()">Login with Google</button>
+    <button>Login with Github</button>
+    <button>Login with Twitter</button>
     <br>
         <p>{{error.message}}</p>
         <br>
@@ -11,6 +11,9 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+import {firebaseApp} from '../firebaseApp'
+
 export default {
   data() {
     return {
@@ -20,11 +23,27 @@ export default {
       }
    },
   methods: {
-    signIn() {
-        firebaseApp.auth().GoogleAuthProvider()
-        .catch(error => {
-            this.error = error
-        })
+    logIn() {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
       }
    }
 }
