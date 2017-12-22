16b9126e308e22e6618f1133cdc8053a13c4b3cc
pankaja-shree <pankajashree@gmail.com>
2017-12-22 12:06:37 +0530

    add router object

--- applications/loginpage/src/main.js
+++ applications/loginpage/src/main.js
@@ -1,8 +1,35 @@
 import Vue from 'vue'
+
 import App from './App.vue'
 import store from './store'
-import router from './router'
+
+import Router from 'vue-router'
+import Home from './components/Home'
+import Login from './components/Login'
+import Signup from './components/Signup'
+//import router from './router'
 import {firebaseApp} from './firebaseApp'
+
+Vue.use(Router)
+
+const router = new Router({
+  mode:'history', //vuerouter has hashmode and history mode , hashmode switches routes with refreshes.
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      component: Signup
+    }
+  ]
+})
 
 //firbase listens if user has signed in or out on any path of the app - always before Vue instance
 firebaseApp.auth().onAuthStateChanged(user => {
@@ -11,7 +38,7 @@ firebaseApp.auth().onAuthStateChanged(user => {
       router.push('/dashboard') //routerpush method will navigate to this page and place this page on top (last of the stack)
   }
   else {
-      router.replace('/signin') //replace will place this page on the beginning of the stack
+      router.replace('/login') //replace will place this page on the beginning of the stack
   }
 })
 
