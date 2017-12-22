501d6a55a1048fba5efaf1abc95a0a4aed384c4b
pankaja-shree <pankajashree@gmail.com>
2017-12-22 16:05:22 +0530

    add link to homepage

--- applications/loginpage/src/components/dashboard.vue
+++ applications/loginpage/src/components/dashboard.vue
@@ -1,9 +1,9 @@
-<template>
+  <template>
   <div id="dashboard">
     <h1>Hello {{$store.state.user.displayName}} </h1>
     <button @click="signOut()">Signout</button>
     <pre>{{$store.state.user}}</pre>
-
+  <router-link to="/">Home</router-link>
   </div>
 </template>
 
