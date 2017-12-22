6ae6602a8e208f06ac30bb7107681e6dce408011
pankaja-shree <pankajashree@gmail.com>
2017-12-22 15:12:16 +0530

    display user object

--- applications/loginpage/src/components/dashboard.vue
+++ applications/loginpage/src/components/dashboard.vue
@@ -1,14 +1,39 @@
 <template>
   <div id="dashboard">
     <h1>Hello {{$store.state.user.displayName}} </h1>
-      <p>{{$store.state}}!</p>
     <button @click="signOut()">Signout</button>
+    <h2>Your profile</h2>
+    <img :src="photo" style=”height: 120px”> <br>
+    <p>{{name}}</p>
+    <p>{{email}}</p>
+    <p>{{userId}}</p>
+    <hr>
+    <pre>{{user}}</pre>
+
   </div>
 </template>
 
 <script>
-import {firebaseApp} from '../firebaseApp'
+import firebase from 'firebase'
 export default {
+  data() {
+    return {
+     photo: '',
+     userId: '',
+     name: '',
+     email: '',
+     user: {}
+   }
+  },
+  created() {
+  this.user = firebase.auth().currentUser;
+  if(this.user) {
+    this.name = this.user.displayName;
+    this.email = this.user.email;
+    this.photo = this.user.photoURL;
+    this.userId = this.user.uid;
+  }
+},
   methods: {
         signOut() {
             this.$store.dispatch('signOut')
