ae27da3b03eb6dce544b711c99ce7818f869914c
Pankajashree <pankajashree@gmail.com>
2017-12-22 09:27:16 +0530

    home page component

---
+++ applications/loginpage/src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <HeaderBar></HeaderBar>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import HeaderBar from './Header'
+export default {
+  name: 'Home',
+  components: {
+    HeaderBar
+  },
+  data () {
+    return {
+      msg: 'Code Stories landing page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
