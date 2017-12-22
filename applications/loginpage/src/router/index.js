8f1c4976a54e37b6592e2f88b014086697674b21
pankaja-shree <pankajashree@gmail.com>
2017-12-22 11:01:49 +0530

    create routes for login and signup

--- applications/loginpage/src/router/index.js
+++ applications/loginpage/src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
 
 Vue.use(Router)
 
 export default new Router({
-  mode:'history', //vuerouter has hashmode and history mode , hashmode switches routes with refreshes. 
+  mode:'history', //vuerouter has hashmode and history mode , hashmode switches routes with refreshes.
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
     },
-    { path: '/signin',
-          component: Signin
+    {
+      path: '/login',
+      component: Login
     },
-    { path: '/signup',
-        component: Signup
+    {
+      path: '/signup',
+      component: Signup
     }
   ]
 })
